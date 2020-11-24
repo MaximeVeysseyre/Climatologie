374288ee535cf6704bf363a0d71e949d36a941fb
Maxime <maxime.veysseyre@gmail.com>
2020-11-24 16:30:54 +0100

    [UPDATE] Requete 1 terminée

--- src/credentials.py
+++ src/credentials.py
@@ -4,3 +4,4 @@ class Credentials():
     RAW_LOCAL_PATH = os.getcwd() + os.sep + 'data' + os.sep + 'RAW' + os.sep
     CURATED_LOCAL_PATH = os.getcwd() + os.sep + 'data' + os.sep + 'CURATED' + os.sep
     TEMPERATURES = r'temperatures.csv'
+    TEMPERATURES_FILTRE = r'temperatures_filtre.csv'
